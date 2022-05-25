31e25fe19716daf469aac90d5f1c604d8c6839a9
mayankkg <102684871+mayankkg@users.noreply.github.com>
2022-05-25 11:42:54 +0530

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'hrm2',
         'USER': 'postgres',
-        'PASSWORD': 'demo1234',
+        'PASSWORD': '<password>',
         'HOST': 'localhost',
     }
 }
